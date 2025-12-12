e6322fe926d8889e5cd88c87d839f19f03e39ed6
Shubhanshu yadav <shubhanshu_y@ce.iitr.ac.in>
2025-12-12 17:34:45 +0530

    removed testing route /me from main.py

--- app/main.py
+++ app/main.py
@@ -3,9 +3,6 @@ from app.db.session import engine, Base
 from app.models import User
 from contextlib import asynccontextmanager
 
-from fastapi import Depends
-from app.api.deps import get_current_user
-
 from app.api.auth import router as auth_router
 from app.api.session_routes import router as session_router
 
@@ -23,13 +20,6 @@ app = FastAPI(title="Agentic Workflow Companion", lifespan=lifespan)
 app.include_router(auth_router)
 app.include_router(session_router)
 
-@app.get("/me")
-def get_me(user : User = Depends(get_current_user)):
-    return {
-        "id": user.id,
-        "fullname": user.fullname,
-        "email": user.email
-    }
 
 @app.get("/health")
 def health_check():
